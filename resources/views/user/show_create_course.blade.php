0111cfcb083bab01488c0f31eb52197c0ed696c8
2018rakibuzzaman <55857298+2018rakibuzzaman@users.noreply.github.com>
2021-03-30 22:33:25 +0600

    create cource

--- resources/views/user/show_create_course.blade.php
+++ resources/views/user/show_create_course.blade.php
@@ -19,201 +19,161 @@
 							<a href="{{asset('/')}}user/create-course">Create Course</a>	
 						</span>
 					</li>
-                </ul>
-            </div>
-        </div>
+        </ul>
+      </div>
+    </div>
 	</div>
 
 	<div class="container">
 		<div class="as_profiel_section">
-
-		<div class="tab_section_button">
-			<div class="container">
-				<div class="as_tab_section">
-					<ul class="clearfix">
-						<li class="active_tab"><a href="{{asset('/')}}user/create-course">Create course</a></li>
-						<li><a href="{{asset('/')}}user/my-course">My course</a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-
+  		<div class="tab_section_button">
+  			<div class="container">
+  				<div class="as_tab_section">
+  					<ul class="clearfix">
+  						<li class="active_tab"><a href="{{asset('/')}}user/create-course">Create course</a></li>
+  						<li><a href="{{asset('/')}}user/my-course">My course</a></li>
+  					</ul>
+  				</div>
+  			</div>
+  		</div>
 		<!-- start form field section -->
-		<div class="form_fielt">
-			<div class="container">
-	            <div class="as_form_field">
-	                <form id="addListingForm" action="#" method="post" enctype="multipart/form-data">
-	                    @csrf
-	                    <!-- form -tem -->
-	                    <div class="form_item line_hide_form_item">
-	                        @error("title")
-	                        <p class="text-danger">{{ $message }}</p>
-	                        @enderror
-	                         <input name="title" type="text" value="" maxlength="50" placeholder="Title*" onkeyup="title_count(this)" />
-	                         <span class="title_count count_style" style="display: none;"><span class="t_count_res">0</span>/50</span>
-	                    </div>
-	                    <!-- form -- item -->
+		  <div class="form_fielt">
+			  <div class="container">
+          <div class="as_form_field">
+            <form id="addListingForm" action="#" method="post" enctype="multipart/form-data">
+              @csrf
 
-	                    <div class="form_item line_hide_form_item text-center" style="position: relative">
-	                    	<!-- form -tem -->
-	                        @error("thumbnail")
-	                        <p class="text-danger">{{ $message }}</p>
-	                        @enderror
+              <!-- Title -->
+              <div class="form_item line_hide_form_item">
+                  @error("title")
+                  <p class="text-danger">{{ $message }}</p>
+                  @enderror
+                   <input name="title" type="text" value="" maxlength="50" placeholder="Title*" onkeyup="title_count(this)" />
+                   <span class="title_count count_style" style=""><span class="t_count_res">0</span>/50</span>
+              </div>
 
+	            <!-- Image upload -->
+              <div class="form_item line_hide_form_item text-center" style="position: relative">
+                @error("thumbnail")
+                <p class="text-danger">{{ $message }}</p>
+                @enderror
+                <div class="thumbnail thambnail_border_btn">
+                  <input type="text" placeholder="Upload images" id="feture_images_name" value="">
+                  <input type="file" accept="image/*" id="feture_img" name="thumbnail" onchange="loadFile(event);show_image();">
+                  <span class='change_btn' >Select images</span>
+                </div>
+                <div id="output_image" style="display: none;">
+                  <img src="#" id="output" style="width:150px; margin-top: 5px"/>
+                </div>
+                
+              </div>
+              <script type="text/javascript">
+                function show_image() {
+                  document.getElementById('output_image').style.display='block';
+                }
+              </script>
 
-	                         <div class="thumbnail thambnail_border_btn">
+              <!-- Video Link -->
+              <div class="form_item line_hide_form_item">
+                  @error("videoUrl")
+                  <p class="text-danger">{{ $message }}</p>
+                  @enderror
+                   <input class="add_course_youtube_url" name="videoUrl" value=""  type="text" placeholder="Add video. Just paste Youtube video link">
+                  <div class="show_course_video">
 
-
-	                          <input type="text" placeholder="Upload images" id="feture_images_name" value="">
-	                          <input type="file" accept="image/*" id="feture_img" name="thumbnail" onchange="loadFile(event)">
-	                          <span class='change_btn' >Change images</span>
-
-	                        </div>
-	                          <img src="#" id="output"/ style="width:150px; margin-top: 5px">
-
-	                    </div><!-- form -- item -->
-	                    <div class="form_item line_hide_form_item"><!-- form -tem -->
-	                        @error("videoUrl")
-	                        <p class="text-danger">{{ $message }}</p>
-	                        @enderror
-	                         <input class="add_course_youtube_url" name="videoUrl" value=""  type="text" placeholder="Add video. Just paste Youtube video link">
-	                        <div class="show_course_video">
-
-	                        </div>
-	                    </div><!-- form -- item -->
-	                    <div class="form_item line_hide_form_item"><!-- form -tem -->
-	                        @error("description")
-	                        <p class="text-danger">{{ $message }}</p>
-	                        @enderror
-	                         <textarea onkeyup="discription_count(this)" maxlength="200" id="" cols="30" rows="3" name="description" placeholder="Description*"></textarea>
-	                         <span class="descrip_count count_style" style="display: none;"><span class="d_count_res">0</span>/200</span>
-	                    </div><!-- form -- item -->
-	                    <div class="form_item dowupdown_singe_line line_hide_form_item"><!-- form -tem -->
-	                        @error("duration")
-	                        <p class="text-danger">{{ $message }}</p>
-	                        @enderror
-	                          <input type="text" name="duration" value="{{ old('duration') }}" placeholder="course duration. e.g. 15 days"/>
-	                         <select class="js-example-basic-duration" name="duration" id="duration" style="width: 100%;">
-	                            <option></option>
-	                            <option value="slksdkfsjdf">sdsdfsdf</option>
-	                           @for ($i = 1; $i < 100; $i++)
-	                           @if (old("duration") == $i)
-	                           <option selected value="{{ $i }}">{{ $i }} days</option>
-	                           @endif
-	                               <option value="{{ $i }}" >{{ $i }} days</option>
-	                           @endfor
-	                          </select>
-	                        </div><!-- form -- item -->
-	                    <h2 class="Topics">
-	                        Topics you will be covering on:
-	                    </h2>
-
-                    
+                  </div>
+              </div>
 
 
+              <!-- Discription-->
+              <div class="form_item line_hide_form_item">
+                  @error("description")
+                  <p class="text-danger">{{ $message }}</p>
+                  @enderror
+                   <textarea onkeyup="discription_count(this)" maxlength="200" id="" cols="30" rows="3" name="description" placeholder="Description*"></textarea>
+                   <span class="descrip_count count_style" style="display: none;"><span class="d_count_res">0</span>/200</span>
+              </div>
 
-                    <div class="form_item line_hide_form_item"><!-- form -tem -->
-                      <div class="topic_days row">
+              <!-- Duration -->
+              <div class="form_item dowupdown_singe_line line_hide_form_item">
+                @error("duration")
+                <p class="text-danger">{{ $message }}</p>
+                @enderror
+                  <input type="text" name="duration" value="{{ old('duration') }}" placeholder="course duration. e.g. 15 days"/>
+                 <select class="js-example-basic-duration" name="duration" id="duration" style="width: 100%; border: none; color: gray;">
+                    <option selected hidden="" >Duration of course</option>
+                   @for ($i = 1; $i < 100; $i++)
+                   @if (old("duration") == $i)
+                   <option value="{{ $i }}">{{ $i }} days</option>
+                   @endif
+                       <option value="{{ $i }}" >{{ $i }} days</option>
+                   @endfor
+                  </select>
+              </div>
 
-                     
-                          <div class="col-md-6">
-                              <div class="form_item line_hide_form_item">
-                              <input type="text"  name="" value="">
-                                </div>
-                            </div>
-            
-
+              <!-- Class Topic-->
+              <h2 class="Topics">
+                  Topics you will be covering on:
+              </h2>
+              <div class="form_item line_hide_form_item">
+                <div class="topic_days row">
+                    <div class="col-md-6">
+                      <div class="form_item line_hide_form_item">
+                        <input type="text"  name="" value="">
                       </div>
-
-                    </div><!-- form -- item -->
-                    <div class="form_item line_hide_form_item"><!-- form -tem -->
-                        @error("admissionFee")
-                        <p class="text-danger">{{ $message }}</p>
-                        @enderror
-                        <div class="row">
-                            <div class="col-md-6">
-                                  <input type="text" class="form-control" value="$" readonly>
-                            </div>
-                            <div class="col-md-6">
-                                <input type="text" placeholder="Admission fee" name="admissionFee" value="" />
-
-                            </div>
-                            </div>
-                    </div><!-- form -- item -->
-                    <div class="switch_box">
-                        @error('offerPercentage')
-                            <p class="text-danger">{{ $message }}</p>
-                        @enderror
-                        <p>Offer discount on first admission
-
-                        </p>
-
-                        <div class="hourly_reat form_item line_hide_form_item discount_position" style=""><!-- form -tem -->
-
-                            <input type="text" maxlength="2"  placeholder="Discount %" name="offerPercentage" value="" style="max-width:">
-                        </div>
-
-                        @error("admission")
-                        <p class="text-danger">{{ $message }}</p>
-                        @enderror
-						<label class="switch" onclick="hourly_red(this)">
-                             <input class="switch_status" type="checkbox" name="admission" />
-                             <span class="slider round"></span>
-                        </label>
-
-
                     </div>
-                    <div class="switch_box" style="display: none">
-                        <p>Do you offer a trial class without booking</p>
-                        @error("booking")
-                        <p class="text-danger">{{ $message }}</p>
-                        @enderror
-                        <label class="switch" >
-                             <input class="switch_status" type="checkbox" name="booking" />
-                             <span class="slider round"></span>
-                        </label>
-                    </div>
-                    <div>
-                        <p style="font-size: 12px">Please select the dates you're available. You can select 3 months ahead at beast. </p>
+                </div>
+              </div>
+
+              <!-- Admission fee-->
+              <div class="form_item line_hide_form_item">
+                  @error("admissionFee")
+                  <p class="text-danger">{{ $message }}</p>
+                  @enderror
+                  <div class="row">
+                      <div class="col-md-6">
+                            <input type="text" class="form-control" value="$" readonly>
                       </div>
+                      <div class="col-md-6">
+                          <input type="text" placeholder="Admission fee" name="admissionFee" value="" />
+                      </div>
+                  </div>
+              </div>
 
-                       <!-- multi date user choice celender section -->
-                       <div class="day_select_calender">
-                           <div style="width: 315px;">
-                               <input style="display: none" id="selectedValues" name="dates" value=""  class="date-values updateValues"/>
-                               <div id="parent">
-                                       <div class="card-header month-selected col-sm" id="monthAndYear">
-                                       </div>
+              <!-- Discount -->
+              <div class="switch_box">
+                  @error('offerPercentage')
+                      <p class="text-danger">{{ $message }}</p>
+                  @enderror
+                  <p>Discount on first admission</p>
+                  <div class="hourly_reat form_item line_hide_form_item discount_position" style="">
+                      <input type="text" maxlength="2"  placeholder="Discount %" name="offerPercentage" value="" style="">
+                  </div>
+                  @error("admission")
+                  <p class="text-danger">{{ $message }}</p>
+                  @enderror
+			            <label class="switch" onclick="hourly_red(this)">
+                       <input class="switch_status" type="checkbox" name="admission" />
+                       <span class="slider round"></span>
+                  </label>
+              </div>
+              <div class="switch_box" style="display: none">
+                  <p>Do you offer a trial class without booking</p>
+                  @error("booking")
+                  <p class="text-danger">{{ $message }}</p>
+                  @enderror
+                  <label class="switch" >
+                       <input class="switch_status" type="checkbox" name="booking" />
+                       <span class="slider round"></span>
+                  </label>
+              </div>
+                       
+              <div class="button_section text-center">
+                <button type="submit" class="button enable-ripple">Update</button>
+              </div>
 
-                                   <div class="custome_day_year_month">
-                                     <select class=" col-xs-6" name="month" id="month" onchange="change()"></select>
-                                     <select class="col-xs-6" name="year" id="year" onchange="change()"></select>
-                                   </div>
-                                   <div class="calender_value">
-                                         <table id="calendar">
-                                       <thead>
-                                           <tr>
-                                               <th>Su</th>
-                                               <th>Mo</th>
-                                               <th>Tu</th>
-                                               <th>We</th>
-                                               <th>Th</th>
-                                               <th>Fr</th>
-                                               <th>Sa</th>
-                                           </tr>
-                                       </thead>
-                                       <tbody id="calendarBody"></tbody>
-                                   </table>
-                                   </div>
-                               </div>
-                           </div>
-                       </div>
-                    <div class="button_section text-center">
-                         <button type="submit" class="button enable-ripple">Update</button>
-
-                    </div>
-                </form>
-            </div>
+            </form>
+          </div>
         </div>
 
     </div><!-- end form field section -->
@@ -222,15 +182,6 @@
     <script>
        document.forms['addListingForm'].elements['duration'].value="";
     </script>
-
-
-
-
-
-
-
-
-
       <!-- upload images query -->
       <script>
           var loadFile = function(event) {
